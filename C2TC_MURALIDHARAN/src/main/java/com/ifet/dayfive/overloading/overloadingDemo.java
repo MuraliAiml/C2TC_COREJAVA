f78fc2082494b41b8e49f2c23c2b28604b48ba86
JAGADHISH <JAGADHISH@Jagadiswaran>
2024-11-08 12:51:51 +0530

    Murali Commit

---
+++ C2TC_MURALIDHARAN/src/main/java/com/ifet/dayfive/overloading/overloadingDemo.java
@@ -0,0 +1,12 @@
+package com.ifet.dayfive.overloading;
+
+public class overloadingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(MethodOverloading.add(1,5));
+		System.out.println(MethodOverloading.add("Murali","dharan"));
+		System.out.println(MethodOverloading.add(2.9f, 1.1f));
+	}
+
+}
---
+++ C2TC_MURALIDHARAN/src/main/java/com/ifet/dayfive/overriding/MethodOverriding.java
@@ -0,0 +1,5 @@
+package com.ifet.dayfive.overriding;
+
+public class MethodOverriding {
+	
+}
